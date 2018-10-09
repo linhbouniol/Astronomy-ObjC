5f2f4c6d668278093c77eb0121f0c4560b2a2dbb
Linh Bouniol <paulalinh89@gmail.com>
2018-10-09 12:52:03 -0700

    Initial Commit

---
+++ Astronomy ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astronomy ObjC
+//
+//  Created by Linh Bouniol on 10/9/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Astronomy ObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Astronomy ObjC
+//
+//  Created by Linh Bouniol on 10/9/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
